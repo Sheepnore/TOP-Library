d9de9b7c5d0fbdc538a628295e6494f262448196
Sheepnore <a83840066@gmail.com>
2024-09-18 22:20:12 +0800

    added basic styling, create renderBooklist(),

--- index.js
+++ index.js
@@ -1,45 +1,86 @@
-const showBookDialogButton = document.querySelector('.showBookDialogButton');
-const bookDialog = document.getElementById('bookDialog');
-const closeDialogButton = document.querySelector('.closeDialogButton');
-const addBookToListButton = document.querySelector('.addToList'); 
-
-let bookLibrary = [];
-
-function Book(name,author,pages){
-  this.bookName = name;
-  this.author = author;
-  this.pages = pages;
-}
-
-addBookToListButton.addEventListener('click',()=>{
-  const inputBookName = document.querySelector('#bookName');
-  const name = inputBookName.value;
-
-  const inputAuthor = document.querySelector('#author');
-  const author = inputAuthor.value;
-
-  const inputPages = document.querySelector('#pages');
-  const pages = inputPages.value;
-
-  const data = new Book(name,author,pages);
-  bookLibrary.push(data);
-})
 
 
+function renderPage(){
 
-// Button default behavior is submit, use preventDefault to cancel the default
-function preventButtonDefault(event){
-  event.preventDefault();
-}
+  const showBookDialogButton = document.querySelector('.showBookDialogButton');
+  const bookDialog = document.getElementById('bookDialog');
+  const closeDialogButton = document.querySelector('.closeDialogButton');
+  const addBookToListButton = document.querySelector('.addToList'); 
+  const books = document.querySelectorAll('.book');
+  const ulElement = document.querySelector('ul');
+  let ulHtml = ulElement.innerHTML;
 
-showBookDialogButton.addEventListener('click',()=>{
-  bookDialog.showModal();
-})
+  let bookLibrary = [
+    {bookName: 'book1',
+     author: 'unknown',
+     pages: 100
+    },
+    {bookName: 'book2',
+     author: 'unknown',
+     pages: 100
+    },
+    {bookName: 'book2',
+     author: 'unknown',
+     pages: 100
+    },
+  ];
 
-closeDialogButton.addEventListener('click', ()=>{
-  bookDialog.close();
-})
+  bookLibrary.forEach((obj)=>{
+    renderBookList(obj);  
+  });
 
-closeDialogButton.addEventListener('click', preventButtonDefault);
+  function renderBookList(book){
+    ulHtml += `
+      <li class="book">
+        Book: ${book.bookName}
+        Author: ${book.author}
+        Pages: ${book.pages}
+      </li>  
+    `
+    ulElement.innerHTML = ulHtml;
+  }
+  
+  function Book(name,author,pages){
+    this.bookName = name;
+    this.author = author;
+    this.pages = pages;
+  }
+  
+  addBookToListButton.addEventListener('click',()=>{
+    const inputBookName = document.querySelector('#bookName');
+    const name = inputBookName.value;
+  
+    const inputAuthor = document.querySelector('#author');
+    const author = inputAuthor.value;
+  
+    const inputPages = document.querySelector('#pages');
+    const pages = inputPages.value;
+  
+    const data = new Book(name,author,pages);
+    bookLibrary.push(data);
 
-addBookToListButton.addEventListener('click', preventButtonDefault);
+    renderBookList(data);
+  })
+  
+  // Button default behavior is submit, use preventDefault to cancel the default
+  function preventButtonDefault(event){
+    event.preventDefault();
+  }
+  
+  showBookDialogButton.addEventListener('click',()=>{
+    bookDialog.showModal();
+  })
+  
+  closeDialogButton.addEventListener('click', ()=>{
+    bookDialog.close();
+  })
+  
+  closeDialogButton.addEventListener('click', preventButtonDefault);
+  
+  addBookToListButton.addEventListener('click', preventButtonDefault);
+
+};
+
+renderPage();
+
+
